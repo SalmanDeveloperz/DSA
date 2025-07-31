a829a2f8b7049d5e2e48f9a173045c5dd3a227c1
Muhammad Salman <chsalmanramzan422@gmail.com>
2025-07-31 21:48:27 +0500

    Adding Time and Space Complexity Docs (Day 3)

--- loops.cpp
+++ loops.cpp
@@ -80,27 +80,27 @@
 /*
 Input: 4
 Output:
-1
-12
-123
-1234
+****
+***
+**
+*
 */
 
 
-// # include <bits/stdc++.h>
-// using namespace std;
+# include <bits/stdc++.h>
+using namespace std;
 
-// int main(){
-//     int n;
-//     cin>> n;
-//     for (int i=n; i>=0; i--){
-//         for (int j=1; j<=i; j++){
-//             cout<<"*";
-//         }
-//         cout<< endl;
-//     }
+int main(){
+    int n;
+    cin>> n;
+    for (int i=n; i>=0; i--){
+        for (int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<< endl;
+    }
 
-// }
+}
 
 
 
@@ -117,17 +117,18 @@ Output:
 */
 
 
-# include <bits/stdc++.h>
-using namespace std;
+// # include <bits/stdc++.h>
+// using namespace std;
 
-int main(){
-    int n;
-    cin>> n;
-    for (int i=0; i<=n; i++){
-        for (int j=i; j<=n; j--){
-            cout<< j;
-        }
-        cout<<endl;
-    }
+// int main(){
+//     int n;
+//     cin>> n;
+//     for (int i=0; i>=n; i++){
+//         for (int j=i; j>=n; j++){
+//             cout<< j;
+//         }
+//         cout<<endl;
+//     }
 
-}
+//     return 0;
+// }
