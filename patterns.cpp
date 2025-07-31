c13906cf4e81fff8c60874a2dd07f7046f50effd
Muhammad Salman <chsalmanramzan422@gmail.com>
2025-08-01 00:21:03 +0500

    Added new pattern

--- patterns.cpp
+++ patterns.cpp
@@ -96,12 +96,47 @@ void pattern5(int n)
     }
 }
 
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+ */
+void pattern6(int n)
+{
+    
+    for (int i = 0; i < n; i++)
+    {
+        // for space
+        for (int j = 0; j < n-i-1; j++){
+            cout << " ";
+        }
+
+        // for stars
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            cout << "*";
+        }
+
+        // for space
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+
+}
+
+
 int main()
 {
     int n;
     cin >> n;
 
-    pattern5(n);
+    pattern6(n);
 
     return 0;
 }
