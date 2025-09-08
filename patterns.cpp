a3173ff2dbbaca635ccc35849d72a7e8a09317af
Muhammad Salman <chsalmanramzan422@gmail.com>
2025-09-08 07:42:56 +0500

    Added Output as a comment above the pattern 12

--- patterns.cpp
+++ patterns.cpp
@@ -330,6 +330,16 @@ void pattern11(int n){
 
 }
 
+/*
+
+    1 
+    2 3 
+    4 5 6 
+    7 8 9 10 
+    11 12 13 14 15 
+
+*/
+
 void pattern12(int n){
     //🎉🎉🎉🎉🎉🎉🎉 Self made Logic Hurraaaaaahhhhhhh Bro 🎉🎉🎉🎉🎉🎉🎉🎉
     int start=1;
