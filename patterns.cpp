a48f540f1c2c4f274312ea70df5e160e778346fd
SalmanDeveloperz <chsalmanramzan422@gmail.com>
2025-12-30 07:17:10 +0500

    Reached to where I left, Pattern # 16
    Signed-off-by: SalmanDeveloperz <chsalmanramzan422@gmail.com>

--- patterns.cpp
+++ patterns.cpp
@@ -448,8 +448,8 @@ void pattern (int n){
 
     // int count= 1;
     for (int i=1; i<=n; i++){
-        for (int j=i; j<=n; j++){
-            cout<<char('A'+j-1);
+        for (int j=1; j<=i; j++){
+            cout<<char('A'+i-1);
         }
         cout<<endl;
 
