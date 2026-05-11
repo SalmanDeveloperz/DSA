4cbaafeee25a29879031969cf30b671c83e6f3f1
SalmanDeveloperz <chsalmanramzan422@gmail.com>
2026-05-11 14:37:10 +0500

    Added brute force and optimal for question # 11
    Signed-off-by: SalmanDeveloperz <chsalmanramzan422@gmail.com>

--- Neetcode150/11. Two Sum - Input Array is Sorted.cpp
+++ Neetcode150/11. Two Sum - Input Array is Sorted.cpp
@@ -1,3 +1,5 @@
+/// Brute force solution with two nested loops
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
@@ -11,3 +13,36 @@ public:
         }
     }
 };
+
+// T.C is O(n^2) and S.C is O(1)
+
+
+
+
+///  Optimal soultion with two pointer approach
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        //two pointers approach
+        int left= 0;
+        int right= numbers. size()-1;
+
+        while (left< right){
+            int sum= numbers[left]+ numbers[right];
+            if (sum== target){
+                return {left+1, right+1};
+            }
+            else if (sum< target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return {};
+
+    }
+};
+
+
+// time complexity of this solution is O(n) and space complexity is O(1)
